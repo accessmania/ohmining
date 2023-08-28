5236e6462ddcc594e7bda84a13ccbe0f1266baab
AccessMania <67264460+accessmania@users.noreply.github.com>
2023-08-28 18:32:19 +0900

    Update regex_entry.py

--- regex_entry.py
+++ regex_entry.py
@@ -3,7 +3,13 @@ import regex
 
 
 
+
+
 vpatterexcel    =   'C:/Users/soshi/Desktop/20230810_regex_안치형/pattern_dict.xlsx'
 vrowdataexcel   =   'C:/Users/soshi/Desktop/20230810_regex_안치형/rawdata.xlsx'    
-            
+
 regex.run(vpatterexcel , vrowdataexcel , '병리결과' ,10000 )    
+
+
+
+
